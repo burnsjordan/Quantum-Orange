1aab9fca71ce7fc251981043b5b521a0af22e729
Jordan Burns <burns.s.jordan@gmail.com>
2019-06-13 16:34:25 -0600

    Add CNOT and TONC to gates

--- gates.py
+++ gates.py
@@ -4,8 +4,8 @@ import numpy as np
 small_dict = {
     'Identity': np.array([[1, 0], [0, 1]]),
     'X': np.array([[0, 1], [1, 0]]),
-    # 'CNOT': np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, 1], [0, 0, 1, 0]]),
-    # 'TONC': np.array([[0, 1, 0, 0], [1, 0, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]]),
+    'CNOT': np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, 1], [0, 0, 1, 0]]),
+    'TONC': np.array([[0, 1, 0, 0], [1, 0, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]]),
     'Hadamard': (1/(2**(1/2)))*np.array([[1, 1], [1, -1]]),
     'Pi8': np.array([[1, 0], [0, np.exp(1j*np.pi/4)]])
 }
@@ -25,16 +25,28 @@ def get_gates(N, list_size):
         count = 0
         for i in small_dict:
             for j in range(1, N+1):
+                marker = 0
                 if(j == 1):
                     temp_matrix = small_dict[i]
+                    if(i == 'CNOT' or i == 'TONC'):
+                        marker = 1
                 else:
                     temp_matrix = small_dict['Identity']
                 identity = small_dict['Identity']
-                for k in range(2, N+1):
+                k = 2 + marker
+                double = False
+                while(k < N+1):
                     if(k == j):
-                        temp_matrix = np.kron(temp_matrix, small_dict[i])
+                        if(k == N and (i == 'CNOT' or i == 'TONC')):
+                            double = True
+                        else:
+                            temp_matrix = np.kron(temp_matrix, small_dict[i])
+                        if(i == 'CNOT' or i == 'TONC'):
+                            k += 1
                     else:
                         temp_matrix = np.kron(temp_matrix, identity)
-                list[count] = temp_matrix
-                count += 1
+                    k += 1
+                if(not double):
+                    list[count] = temp_matrix
+                    count += 1
     return(list)
