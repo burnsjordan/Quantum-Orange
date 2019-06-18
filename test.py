f2eeef2d0bc2a96a095e4c4d8f2805cefacae1a5
Jordan Burns <burns.s.jordan@gmail.com>
2019-06-18 15:48:36 -0600

    Add more tests

--- test.py
+++ test.py
@@ -4,11 +4,18 @@ import learning_functions
 import train_functions
 
 # Setup test matrices
+# Test using an identity matrix
 i_test = np.identity(4)
 current_matrix = np.identity(np.size(i_test, 0))
 gates_list = gates.get_gates(int(np.log2(np.size(current_matrix, 0))), 'small')
 max_depth = 10
 error_threshold = 0.1
+# Test using one gate from the gates list
+one_gate_test = np.dot(learning_functions.random_algo(i_test, 0, gates_list, 0), i_test)
+# Test using five gates from the gates list
+five_gate_test = i_test
+for i in range(5):
+    five_gate_test = np.dot(learning_functions.random_algo(five_gate_test, 0, gates_list, 0), five_gate_test)
 
 def test_algo(target_matrix, current_matrix, trained_ai, learning_function, temp_str):
     high_error = True
@@ -28,21 +35,30 @@ def test_algo(target_matrix, current_matrix, trained_ai, learning_function, temp
 print('')
 print('Testing Random Algorithm:')
 test_algo(i_test, i_test, 0, learning_functions.random_algo, 'i_test: ')
+test_algo(one_gate_test, i_test, 0, learning_functions.random_algo, 'one_gate_test: ')
+test_algo(five_gate_test, i_test, 0, learning_functions.random_algo, 'five_gate_test: ')
 
 
 # Test Greedy Algorithm
 print('')
 print('Testing Greedy Algorithm')
 test_algo(i_test, i_test, 0, learning_functions.greedy_algo, 'i_test: ')
+test_algo(one_gate_test, i_test, 0, learning_functions.greedy_algo, 'one_gate_test: ')
+test_algo(five_gate_test, i_test, 0, learning_functions.greedy_algo, 'five_gate_test: ')
 
 
 # Test Deep Greedy Algorithm
 print('')
 print('Testing Deep Greedy Algorithm')
 test_algo(i_test, i_test, 0, learning_functions.deep_greedy_algo, 'i_test: ')
+test_algo(one_gate_test, i_test, 0, learning_functions.deep_greedy_algo, 'one_gate_test: ')
+test_algo(five_gate_test, i_test, 0, learning_functions.deep_greedy_algo, 'five_gate_test: ')
 
 
 # Test Monte Carlo Algorithm
 print('')
 print('Testing Monte Carlo Algorithm')
 test_algo(i_test, i_test, train_functions.train_monte_carlo(i_test, i_test, gates_list), learning_functions.monte_carlo_algo, 'i_test: ')
+test_algo(one_gate_test, i_test, train_functions.train_monte_carlo(one_gate_test, i_test, gates_list), learning_functions.monte_carlo_algo, 'one_gate_test: ')
+test_algo(five_gate_test, i_test, train_functions.train_monte_carlo(five_gate_test, i_test, gates_list), learning_functions.monte_carlo_algo, 'five_gate_test: ')
+
