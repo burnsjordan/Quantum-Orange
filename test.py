162bc9a36e3a41a67c79b4d192aa08e059ddb724
Jordan Burns <burns.s.jordan@gmail.com>
2019-06-18 17:14:45 -0600

    Add test for random unitary matrix

--- test.py
+++ test.py
@@ -2,13 +2,14 @@ import numpy as np
 import gates
 import learning_functions
 import train_functions
+from scipy.stats import unitary_group
 
 # Setup test matrices
 # Test using an identity matrix
 i_test = np.identity(4)
 current_matrix = np.identity(np.size(i_test, 0))
 gates_list = gates.get_gates(int(np.log2(np.size(current_matrix, 0))), 'small')
-max_depth = 10
+max_depth = 5
 error_threshold = 0.1
 # Test using one gate from the gates list
 one_gate_test = np.dot(learning_functions.random_algo(i_test, 0, gates_list, 0), i_test)
@@ -16,6 +17,8 @@ one_gate_test = np.dot(learning_functions.random_algo(i_test, 0, gates_list, 0),
 five_gate_test = i_test
 for i in range(5):
     five_gate_test = np.dot(learning_functions.random_algo(five_gate_test, 0, gates_list, 0), five_gate_test)
+# Test using random unitary gate
+unitary_test = unitary_group.rvs(2**int(np.log2(np.size(current_matrix, 0))))
 
 def test_algo(target_matrix, current_matrix, trained_ai, learning_function, temp_str):
     high_error = True
@@ -37,6 +40,7 @@ print('Testing Random Algorithm:')
 test_algo(i_test, i_test, 0, learning_functions.random_algo, 'i_test: ')
 test_algo(one_gate_test, i_test, 0, learning_functions.random_algo, 'one_gate_test: ')
 test_algo(five_gate_test, i_test, 0, learning_functions.random_algo, 'five_gate_test: ')
+test_algo(unitary_test, i_test, 0, learning_functions.random_algo, 'unitary_test: ')
 
 
 # Test Greedy Algorithm
@@ -45,6 +49,7 @@ print('Testing Greedy Algorithm')
 test_algo(i_test, i_test, 0, learning_functions.greedy_algo, 'i_test: ')
 test_algo(one_gate_test, i_test, 0, learning_functions.greedy_algo, 'one_gate_test: ')
 test_algo(five_gate_test, i_test, 0, learning_functions.greedy_algo, 'five_gate_test: ')
+test_algo(unitary_test, i_test, 0, learning_functions.greedy_algo, 'unitary_test: ')
 
 
 # Test Deep Greedy Algorithm
@@ -53,12 +58,14 @@ print('Testing Deep Greedy Algorithm')
 test_algo(i_test, i_test, 0, learning_functions.deep_greedy_algo, 'i_test: ')
 test_algo(one_gate_test, i_test, 0, learning_functions.deep_greedy_algo, 'one_gate_test: ')
 test_algo(five_gate_test, i_test, 0, learning_functions.deep_greedy_algo, 'five_gate_test: ')
+test_algo(unitary_test, i_test, 0, learning_functions.deep_greedy_algo, 'unitary_test: ')
 
 
 # Test Monte Carlo Algorithm
 print('')
 print('Testing Monte Carlo Algorithm')
-test_algo(i_test, i_test, train_functions.train_monte_carlo(i_test, i_test, gates_list), learning_functions.monte_carlo_algo, 'i_test: ')
-test_algo(one_gate_test, i_test, train_functions.train_monte_carlo(one_gate_test, i_test, gates_list), learning_functions.monte_carlo_algo, 'one_gate_test: ')
-test_algo(five_gate_test, i_test, train_functions.train_monte_carlo(five_gate_test, i_test, gates_list), learning_functions.monte_carlo_algo, 'five_gate_test: ')
+test_algo(i_test, i_test, train_functions.no_train_monte_carlo(i_test, i_test, gates_list), learning_functions.monte_carlo_algo, 'i_test: ')
+test_algo(one_gate_test, i_test, train_functions.no_train_monte_carlo(one_gate_test, i_test, gates_list), learning_functions.monte_carlo_algo, 'one_gate_test: ')
+test_algo(five_gate_test, i_test, train_functions.no_train_monte_carlo(five_gate_test, i_test, gates_list), learning_functions.monte_carlo_algo, 'five_gate_test: ')
+test_algo(unitary_test, i_test, train_functions.no_train_monte_carlo(unitary_test, i_test, gates_list), learning_functions.monte_carlo_algo, 'unitary_test: ')
 
