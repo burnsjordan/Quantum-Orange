a53c9e874cf23edf1e2038bde0dab1deaadc2905
Jordan Burns <burns.s.jordan@gmail.com>
2019-11-22 14:22:50 -0700

    Remove inverses and identities from generated training data.

--- generate_training_data.py
+++ generate_training_data.py
@@ -3,6 +3,25 @@ import random
 
 Identity = np.array([[1, 0], [0, 1]])
 
+def tuple_array(arr):
+    temp = []
+    temp.append(np.size(arr, 0))
+    for i in range(np.size(arr, 0)):
+        for j in range(np.size(arr, 1)):
+            temp.append(arr[i][j])
+    return tuple(temp)
+
+
+def untuple_array(arr):
+    temp = []
+    N = arr[0]
+    for i in range(N):
+        temp2 = []
+        for j in range(N):
+            temp2.append(arr[N*i+j+1])
+        temp.append(temp2)
+    return np.array(temp)
+
 # Small set of quantum gates
 small_dict = {
     'Identity': np.array([[1, 0], [0, 1]]),
@@ -59,28 +78,46 @@ def get_gates(N, list_size):
 # Produce training data for a 4x4 matrix
 # Returns an array which has the array of inputs as the first element
 # and the array of outputs as the second element
-def get_training_data():
+def get_training_data(N):
     # Setup to build training data
     inputs = []
     outputs = []
-    gates_list = get_gates(2, "small")
+    gates_list = get_gates(N, "small")
+    identity_hit_count = 0
+    inverse_hit_count = 0
 
     # Build training data
     # i*j is the number of data points
     for i in range(200):
         input_matrix = np.kron(Identity, Identity)
         for j in range(50):
-            temp = gates_list[random.choice(list(gates_list.keys()))]
-            input_matrix = np.dot(input_matrix, temp)
-            inputs.append(input_matrix)
-            outputs.append(temp)
+            identity = np.identity(2**N)
+            dont_add = True
+            while(dont_add):
+                dont_add = False
+                temp = gates_list[random.choice(list(gates_list.keys()))]
+                if(tuple_array(temp) == tuple_array(identity)):
+                    dont_add = True
+                    identity_hit_count += 1
+                if(outputs):
+                    if(tuple_array(np.matmul(outputs[-1], temp)) == tuple_array(identity)):
+                        dont_add = True
+                        inverse_hit_count += 1
+                if(not dont_add):
+                    input_matrix = np.dot(input_matrix, temp)
+                    inputs.append(input_matrix)
+                    outputs.append(temp)
 
     # Print the first 10 inputs and outputs if you want
-    """ for i in range(10):
-        print(str(i+1) + ':')
-        print(inputs[i])
-        print(outputs[i]) """
+    # for i in range(10):
+    #     print(str(i+1) + ':')
+    #     print(inputs[i])
+    #     print(outputs[i])
+
+    # Print number of times the identity or an inverse is chosen and ignored
+    # print("identity_hit_count: " + str(identity_hit_count))
+    # print("inverse_hit_count: " + str(inverse_hit_count))
 
     return [inputs, outputs]
 
-get_training_data()
+get_training_data(2)
